f4c58ad27abe78a9160c35d4a3261cd5d1c67377
aviel <aviel469@gmail.com>
2024-11-10 17:37:36 +0200

    root state type and conditioal rendring on the link in the nav bar

--- src/componets/Nav.tsx
+++ src/componets/Nav.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { RootState, useAppSelector } from '../redux/store'
 
 export default function Nav() {
+    const user = useAppSelector((state:RootState) => state.user)
   return (
     <div className='nav'>
+        {user.user ? (<>
+        <NavLink to={"/votes"} >Votes</NavLink>
+        {user.user.isAdmin &&(<NavLink to={"/statistics"} >Statistics</NavLink>)}
+        <button onClick={() => {alert("Log out successfully")}}>logout</button>
+        </>):<>
         <NavLink to={"/login"} >Login</NavLink>
         <NavLink to={"/register"} >Register</NavLink>
-        <NavLink to={"/votes"} >Votes</NavLink>
-        <NavLink to={"/statistics"} >Statistics</NavLink>
-        <button onClick={() => {alert("Log out successfully")}}>logout</button>
+        </>}
     </div>
   )
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./slices/userSlice";
 import candidatesSlice from "./slices/candidatesSlice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 const store  = configureStore({
     reducer:{
@@ -8,4 +10,8 @@ const store  = configureStore({
         candidates:candidatesSlice.reducer
     }
 })
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
 export default store
